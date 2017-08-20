33b9f619424e745009c085231c7bdb8e04b2c988
User-PC\User <shimax15@gmail.com>
2017-08-20 18:40:11 +0500

    ajax call

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,12 @@
 $(function() {
 
   // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/1614476.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      // handle response
+    }
+  });
 
 });
